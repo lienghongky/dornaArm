1a9d244bbcfb34e63fa82fc4079f11055c0e9598
Hongky <hongkypcx.hp@gmail.com>
2020-04-07 16:56:42 +0700

    adding adjust ment

--- dorna_wrapper.py
+++ dorna_wrapper.py
@@ -1,5 +1,5 @@
 from dorna import Dorna
-
+from copy import deepcopy
 import time
 import datetime
 
@@ -8,9 +8,16 @@ import json
 import sys
 import os
 import platform
-
+import enum
 import configparser
 
+class Direction(enum.Enum):
+    FORWARD = 'x'
+    BACKWARD = '-x'
+    RIGHT = 'y'
+    LEFT = '-y'
+    UP = 'z'
+    DOWN = '-z'
 class pose:
     PATH_JOINT = 'joint'
     PATH_LINE = 'line'
@@ -62,13 +69,14 @@ class arm:
 
     JOINTS = ['j0','j1','j2','j3','j4']
     AXISES = ['x','y','z','a','b']
+    PRM_TEMPLATE ={'path':'joint','movement':1}
 
     # Utility poses
 
     HOME_POSE = pose([0,135,-90,-45,0],"the HOME (resting) position",SPACE_JOINT)
     FLAT_POSE = pose([0,0,0,0,0],"the FLAT (outstretched) position",SPACE_JOINT)
     STANDING_POSE = pose([0,90,0,0,0],"the STANDING (vertical) position",SPACE_JOINT)
-    
+    J04_CONFIG_POSE = pose([0,15,-105,0,0],"the FLAT (outstretched) position",SPACE_JOINT)
     # Initialisation
 
     def __init__(self,port=None,config=None):
@@ -190,4 +198,160 @@ class arm:
         return
 
     # TODO Adjustment function
+ 
+    def setJ04Zero(self):
+        """
+        SET ALL JOINTS TO ZERO AND SAVE CONFIG FILE TO THE ONE IN USED
+        SHOULD ONLY BE CALL WHEN THE ARM IS IN FlAT POSITION(MANUALLY OR COMMENT)
+        """
+        self.robot.set_joint({'j0':0})
+        self.robot.set_joint({'j4':0})
+        self.robot.save_config()     
+    def setAllZero(self):
+        """
+        SET ALL JOINTS TO ZERO AND SAVE CONFIG FILE TO THE ONE IN USED
+        SHOULD ONLY BE CALL WHEN THE ARM IS IN FlAT POSITION(MANUALLY OR COMMENT)
+        """
+        self.robot.set_joint([0,0,0,0,0])
+        self.robot.save_config()
+        
+    def isConnected(self):
+        device = json.loads(self.robot.device())
+        if device['connection'] == 2:
+            return True
+        else:
+            return False
+    def isHomed(self):
+        if not self.isConnected():
+            print("Robot is not connected")
+            return False
+            
+        homed = json.loads(self.robot.homed())
+        for key in homed:
+            if homed[key] == 1:
+                print(key," is homed")
+            else:
+                print(key," is not homed yet")
+    def go(self,position):
+            print("Go to ",position)
+            tmpcmd={"command":"move", "prm":position}
+            self.robot.play(tmpcmd)
+            
+    #pass in joint and value as parameter ex:adjust("j0",10)
+    def adjustJoint(self,joint, degrees:float,relatively=True):
+        """
+        ADJUST the joints
+        """
+        # creates a position-movement description that can be fed to go().
+        
+        tempPath=deepcopy(self.PRM_TEMPLATE)
+        for j in self.JOINTS:
+            if j == joint:
+                
+                tempPath[j]=degrees
+                break
+        tempPath["movement"]= 1 if relatively else 0
+        self.go(tempPath)
+        return tempPath  
+
+    #pass in joints object as parameter ex:adjust({"j1":0,"j2":0})
+    def adjustJoints(self,joints,relatively=True):
+        # creates a position-movement description that can be fed to go().
+        tempPath=deepcopy(self.PRM_TEMPLATE)
+        print(joints)
+        for key in joints:
+            for validJoint in JOINTS:
+                if key == validJoint:
+                    tempPath[validJoint]=joints[key]
+                
+        tempPath["movement"]= 1 if relatively else 0
+        self.go(tempPath)
+        return tempPath
+
+        
+    """==================================================
+        MOVE with x,y,z Coordinate System
+        =================================================
+    """
+    #pass in axis and value as parameter
+    #ex:moveToPosition('z',10)
+    def adjustAxis(self,axis, degrees,relatively=True):
+        tempPath=deepcopy(self.PRM_TEMPLATE)
+        tempPath['path'] = 'line'
+        
+        for c in self.AXISES:
+            if c == axis:
+                tempPath[c]=degrees
+                break
+        tempPath["movement"]= 1 if relatively else 0
+        self.go(tempPath)
+        return tempPath
     
+    """==================================================
+        MOVE with xyz Coordinate System
+        =================================================
+    """
+    #pass in coordinate object as parameter
+    #ex:moveToCoordinate({'x':10,'y':20,'z':10})
+    def adjustCoordinate(self,coor,relatively=True):
+        tempPath=deepcopy(self.PRM_TEMPLATE)
+        tempPath['path'] = 'line'
+        
+        for key in coor:
+            for validJoint in self.AXISES:
+                if key == validJoint:
+                    tempPath[validJoint]=coor[key]
+        tempPath["movement"]= 1 if relatively else 0            
+        self.go(tempPath)
+        return tempPath
+    """==================================================
+        wait 
+        =================================================
+    """
+    def wait(self,value):
+        time.sleep(value)
+    def goHome(self):
+        self.go(HOME)
+    def goFlat(self):
+        self.go(FLAT)
+    """==================================================
+        MOVE tools head relatively to current angle
+        =================================================
+    """
+    def adjustHead(self,angle):
+        self.adjust('j3',angle)
+        return self.pos()[3]
+    """==================================================
+        MOVE tools head to specific Angle
+        =================================================
+    """
+    def setHeadAngle(self,angle):
+        self.adjust('j3',angle,False)
+        return self.pos()[3]
+    """==================================================
+        MOVE with specific directin
+        =================================================
+    """   
+    def move(self,direct:Direction.FORWARD,value:float):
+        if direct == Direction.FORWARD:
+            self.adjustAxis('x',value)
+        if direct == Direction.BACKWARD:
+            moveAxis('x',value * -1)
+        if direct == Direction.UP:
+            self.adjustAxis('z',value)
+        if direct == Direction.DOWN:
+            self.adjustAxis('z',value * -1)
+        if direct == Direction.LEFT:
+            self.adjustAxis('y',value)
+        if direct == Direction.RIGHT:
+            self.adjustAxis('y',value * -1)
+            
+    """==================================================
+        MOVE move to a coordiation
+        ex:moveTo((1,2,6)) x=1,y=2,z6
+        =================================================
+    """ 
+    def moveTo(self,xyz):
+        self.adjustCoordinate({'x':xyz[0],
+                               'y':xyz[1],
+                               'z':xyz[2]},False)

ce2aa982fc5a76bc5d88c29630e587f64b7e33b0
hongky <hongkypcx.hp@gmail.com>
2020-04-30 15:10:02 +0700

    layout change

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 from flask import Flask,request,render_template
-from dorna_wrapper.dorna_wrapper import Arm
+#from dorna_wrapper.dorna_wrapper import Arm
 app = Flask(__name__)
 arm = Arm()
+@app.route('/get-positions')
+def getPosition():
+    return arm.getAllPositions()
 @app.route('/')
 def index():
-    return reder_template('index.html')
+    return render_template('fixed.html')
 if __name__ == '__main__':
     app.run(debug=True)
